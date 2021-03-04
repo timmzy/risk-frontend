38a28486dadd6c8b0dfdc356374028a960e4c32a
Timmzy <dapwill@gmail.com>
2021-03-04 17:06:44 +0100

    Add enum option

--- src/assets/js/apis.js
+++ src/assets/js/apis.js
@@ -1,10 +1,12 @@
 import {request} from "../../helpers/utils";
+import toastr from 'toastr';
 
 export async function getRisks() {
     try {
         const res = await request.get('risks-fields/');
         return res
     } catch (e) {
+        toastr.error("Something went wrong")
         // Error
     }
 
